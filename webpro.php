ff1873fbcab6d7b3f8c308f8d05ffd4a63892e26
Kevin Andrea Saputra <keandreas16@gmail.com>
2024-03-30 15:17:30 +0700

    Upload tugas

---
+++ webpro.php
@@ -0,0 +1,16 @@
+<?php
+
+$module = (isset($_GET['m'])) ? $_GET['m'] : 'home';
+
+switch ($module) {
+    case 'home':
+        include_once('home.php');
+        break;
+    case 'kelas':
+        include_once('kelas/index.php');
+        break;
+    case 'siswa':
+        include_once('siswa/index.php');
+        break;
+        break;
+}
